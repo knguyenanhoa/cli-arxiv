40cb6fc562dfb91bf5261d9fe5edea2815fe1e4f
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2020-04-30 13:55:26 +0700

    Add licensing information to source files

--- menus/search.py
+++ menus/search.py
@@ -1,3 +1,26 @@
+"""
+ArXiv CLI Browser - a command line interface browser for the popular pre-print
+academic paper website https://arxiv.org
+
+Copyright (C) 2020  Klinkesorn Nguyen An Hoa
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Author contactable at k<dot>nguyen<dot>an<dot>hoa<at>gmail<dot>com
+"""
+
 import sys, time, os, copy
 
 from components import navigable_menus, arxiv_api
